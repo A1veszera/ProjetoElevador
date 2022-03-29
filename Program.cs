d558857a96e9496a1392d877c6af922da8bb687a
Henrique Alves <99695063+A1veszera@users.noreply.github.com>
2022-03-28 23:02:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    class Program : Elevador
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Projeto Elevador!");
+
+            //Chama o metodo de inicializar
+            Inicializar();
+        }
+    }
+}
